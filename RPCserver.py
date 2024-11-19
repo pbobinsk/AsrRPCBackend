5c47cd73aa302d68c59f3f481e525964395d960c
pbobinsk <pbobinsk@gmail.com>
2024-11-19 10:40:52 +0100

    kopiowanie csv do openAI

--- RPCserver.py
+++ RPCserver.py
@@ -1,8 +1,9 @@
 import os, argparse, json
 from dotenv import load_dotenv
 from jsonrpcserver import Success, method, serve, Result, Error
-import logging
+import logging, traceback
 import shutil
+from pathlib import Path
 
 load_dotenv()
 import sys
@@ -15,6 +16,9 @@ logging.basicConfig(
 )
 PORT = os.getenv('PORT',5000)
 nameSpaceArgs= argparse.Namespace(**{"directory":WORKING_DIR})
+
+PRODUCTION = os.getenv('PRODUCTION',1)
+OPENAI_DIR = os.getenv('OPENAI_DIR')
 
 @method
 def ping():
@@ -43,11 +47,21 @@ def doASR(file: str, file_doctor) -> Result:
         setattr(nameSpaceArgs,'audio_name',file)
         setattr(nameSpaceArgs,'audio_name_doctor',file_doctor)
         asr.main(nameSpaceArgs)
-        audio_path = os.path.join(WORKING_DIR, "WR_S0001_Z05BO.wav.json")
         json_file_path = os.path.join(WORKING_DIR, f"{nameSpaceArgs.audio_name}.json")
-        shutil.copy(audio_path,json_file_path)
+        csv_file_path = os.path.join(WORKING_DIR, f"{Path(nameSpaceArgs.audio_name).stem}.csv")
+        # Symulacja ASR, tylko do testów lokalnych, normalnie json i csv będą wygenerowane jak powyżej
+        if PRODUCTION == 0:
+            json_gen_path = os.path.join(WORKING_DIR, "WR_S0001_Z05BO.wav.json")
+            csv_gen_path = os.path.join(WORKING_DIR, "WR_S0001_Z05BO.csv")
+            shutil.copy(json_gen_path,json_file_path)
+            shutil.copy(csv_gen_path,csv_file_path)
+            logging.info(json_file_path)
+            logging.info(csv_file_path)
+        #koniec syulacji działania ASR
         f = open(json_file_path, encoding='utf-8')
         data = json.load(f)
+        csv_openai_path = os.path.join(OPENAI_DIR, Path(csv_file_path).name)
+        shutil.copy(csv_file_path,csv_openai_path)
     except Exception as e:
         logging.error('Error during ASR '+str(e))
         return Error(-1,str(e))
